d54b170c82d103c049fbf39c1ec08846965b929a
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-17 14:02:32 -0600

    date time logical expressions part 1

--- Logical-expresssions/Logical_expressions.php
+++ Logical-expresssions/Logical_expressions.php
@@ -4,6 +4,18 @@
     <title>Document</title>
   </head>
   <body>
+    <?php
+        $date = date('F',time());
+        echo $date;
+        ?>
+        <br/>
+        <?php
+        if ($date =="August"){
+            echo "It's August, so it's really hot.";
+        }else{
+            echo "Not August, so at least not in the peak of the heat.";
+        }
+    ?>
 
 
 
@@ -12,6 +24,5 @@
 
 
 
-  
   </body>
 </html>
